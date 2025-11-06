18cc2ee019f5297332d0e28f3e7723ffd8f1ce4c
ABHI <96046548+SINHA-IIT@users.noreply.github.com>
2025-11-06 22:38:29 +0530

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import joblib
-from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import accuracy_score,precision_score, recall_score, f1_score, classification_report
 
 def main():
     df = pd.read_csv('data/data_iris.csv')
@@ -9,6 +9,21 @@ def main():
     model = joblib.load('model/model.pkl')
     preds = model.predict(X)
     acc = accuracy_score(y, preds)
+    prec = precision_score(y, preds, average='weighted')
+    rec = recall_score(y, preds, average='weighted')
+    f1 = f1_score(y, preds, average='weighted')
+
+    metrics = {
+        'accuracy': [acc],
+        'precision': [prec],
+        'recall': [rec],
+        'f1_score': [f1]
+    }
+
+    # Create metrics.csv
+    metrics_df = pd.DataFrame(metrics)
+    metrics_df.to_csv('metrics.csv', index=False)
+    print(metrics_df)
     print("Accuracy:", acc)
     print(classification_report(y, preds))
 
